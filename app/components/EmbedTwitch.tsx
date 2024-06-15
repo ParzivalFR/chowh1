98a90d4a04d5f43f9c04dee2a289defd6c98d5db
ParzivalFR <parzivaleu@gmail.com>
2024-06-15 08:53:12 +0200

    Fix Twitch embed

--- app/components/EmbedTwitch.tsx
+++ app/components/EmbedTwitch.tsx
@@ -2,14 +2,14 @@ const EmbedTwitch = () => {
   return (
     <section className="w-full md:w-4/5 flex flex-col md:flex-row m-auto">
       <iframe
-        src="https://player.twitch.tv/?channel=chowh1&parent=localhost&muted=true"
+        src="https://player.twitch.tv/?channel=chowh1&parent=https://chowh1.vercel.app&muted=true"
         height="300"
         width="500"
         className="w-full md:h-96 xl:h-[500px]  md:max-w-[800px]"
         allowFullScreen
       ></iframe>
       <iframe
-        src="https://www.twitch.tv/embed/chowh1/chat?parent=localhost&darkpopout"
+        src="https://www.twitch.tv/embed/chowh1/chat?parent=https://chowh1.vercel.app&darkpopout"
         height="300"
         width="250"
         className="w-full md:w-1/3 md:h-96 xl:h-[500px] max-w-1/3"
